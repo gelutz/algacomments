189a3f933de1c293cf3f8ff85c577301d00d8bb0
gelutz <gabriel.lutz5@gmail.com>
2025-11-12 21:54:52 -0300

    feat: commit inicial

---
+++ moderation-service/src/test/java/com/lutz/alga/CommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutz.alga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
